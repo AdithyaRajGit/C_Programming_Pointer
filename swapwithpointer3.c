56928944a4cd792330d94d968ce1f22edcc036a5
Adithya Raj <159401209+AdithyaRajGit@users.noreply.github.com>
2024-12-11 22:59:58 +0530

    new programme

--- swapwithpointer3.c
+++ swapwithpointer3.c
@@ -1,15 +1,25 @@
 #include<stdio.h>
-void swap(int *x, int *y){
-int temp=*x;
-*x=*y;
-*y=temp;
+#include <string.h>
+void swap(char *x, char *y){
+  int temp=*x;
+  *x=*y;
+  *y=temp;
+  }
+
+void stringRev(char *st, int front, int end){
+  if(front>=end) return;
+  swap(&st[front], &st[end]);
+  stringRev(st, front+1, end-1);
 }
 
-
 int main(){
-int a =10, b=20;
-printf("Before swap : a=%d, b=%d \n",a,b);
-swap(&a,&b);
-printf("After swap : a=%d, b=%d \n",a,b);
+char st[100];
+printf("Enter the string: ");
+  fgets(st,100,stdin);
+  st[strcspn(st,"\n")]='\0';
+  printf("Entered string: %s\n",st);
+
+stringRev(st,0,strlen(st)-1);
+ printf("Swapped string: %s\n",st);
 return 0;
 }
